1ba7fa29fac4a2ccd769ee0f5cd46e0093e4048e
Andrew Braunstein <awb@covariant.ai>
2022-11-30 21:08:54 -0800

    day 1

--- 2022/days/1.py
+++ 2022/days/1.py
@@ -1,1 +1,8 @@
 from common import data
+
+elves = data(1, sep="\n\n")
+
+calories_per_elf = [sum(map(int, elf.split("\n"))) for elf in elves]
+
+print(max(calories_per_elf))
+print(sum(sorted(calories_per_elf, reverse=True)[:3]))
