c4c180e03971ec0c63bba541d2902fde55489367
Andrew Braunstein <awb@covariant.ai>
2022-12-02 21:28:03 -0800

    day 3

---
+++ 2022/days/3.py
@@ -0,0 +1,34 @@
+import functools
+from common import data
+
+sacks = data(3, sep="\n")
+sacks_with_compartments = []
+for sack in sacks:
+    sacks_with_compartments.append((sack[: len(sack) // 2], sack[len(sack) // 2 :]))
+
+overlapping_items = []
+for sack in sacks_with_compartments:
+    overlapping_items.append(set(sack[0]).intersection(set(sack[1])).pop())
+
+
+def priority(item_type: str) -> int:
+    assert len(item_type) == 1
+    item_type_ord = ord(item_type)
+    if item_type_ord >= ord("a") and item_type_ord <= ord("z"):
+        return (item_type_ord - ord("a")) + 1
+    else:
+        return (item_type_ord - ord("A")) + 27
+
+
+print(sum(map(priority, overlapping_items)))
+
+total = 0
+i = 0
+while i < len(sacks):
+    group = []
+    for j in range(3):
+        group.append(set(sacks[i]))
+        i += 1
+    total += priority(functools.reduce(lambda s1, s2: s1.intersection(s2), group).pop())
+
+print(total)
