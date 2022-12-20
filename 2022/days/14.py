3cc6ab66488f090da83f0cf125b91287b31f01a9
Andrew Braunstein <awb@covariant.ai>
2022-12-20 00:12:38 -0800

    refactor

--- 2022/days/14.py
+++ 2022/days/14.py
@@ -31,8 +31,8 @@ def print_board(board):
     print("\n".join(" ".join(line) for line in board))
 
 
-def part1():
-    board = [["." for _ in range(max_col + 2)] for _ in range(max_row + 2)]
+def build_board():
+    board = [["." for _ in range(max_col + 200)] for _ in range((max_row + 2) + 1)]
 
     for line in lines:
         cursor = line[0]
@@ -49,6 +49,12 @@ def part1():
                 board[r][c_start] = "#"
 
             cursor = position
+
+    return board
+
+
+def part1():
+    board = build_board()
 
     sand_drop = (500, 0)
     sand_count = 0
@@ -73,23 +79,7 @@ def part1():
 
 
 def part2():
-    board = [["." for _ in range(max_col + 200)] for _ in range((max_row + 2) + 1)]
-
-    for line in lines:
-        cursor = line[0]
-        for position in line[1:]:
-            cursor_c, cursor_r = cursor
-            position_c, position_r = position
-            c_diff = abs(cursor_c - position_c) + 1
-            r_diff = abs(cursor_r - position_r) + 1
-            c_start = min(cursor_c, position_c)
-            r_start = min(cursor_r, position_r)
-            for c in range(c_start, c_start + c_diff):
-                board[r_start][c] = "#"
-            for r in range(r_start, r_start + r_diff):
-                board[r][c_start] = "#"
-
-            cursor = position
+    board = build_board()
 
     sand_drop = (500, 0)
     sand_count = 0
